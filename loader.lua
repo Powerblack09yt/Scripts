8a2c20c950d2edb5fa03b4fb4266af86736830e8
Caveira1010 <127628592+Powerblack09yt@users.noreply.github.com>
2024-08-17 15:54:33 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,8 @@
 local placeId = game.PlaceId
 if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
     BF = true
-elseif placeId == 6299805723 then
-    AFS = true
-
-    print("\n game not support")
+elseif placeId == 10260193230 then
+    MS = true
 end
 spawn(function()
 	while wait() do 
@@ -13,6 +11,6 @@ end)
 if BF then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Powerblack09yt/Scripts/main/Caveirahub-Bf.lua"))()
 end
-if AFS then
-print("em breve")
+if MS then
+
 end
