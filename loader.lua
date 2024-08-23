46c22ee7c1f7b12f94355278b23c04b8da2d788d
Caveira1010 <127628592+Powerblack09yt@users.noreply.github.com>
2024-08-23 13:13:01 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,8 @@
 local placeId = game.PlaceId
 if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
     BF = true
-elseif placeId == 10260193230 then
-    MS = true
+elseif placeId == 12411473842 then
+    press = true
 end
 spawn(function()
 	while wait() do 
@@ -11,6 +11,6 @@ end)
 if BF then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Powerblack09yt/Scripts/main/Caveirahub-Bf.lua"))()
 end
-if MS then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Powerblack09yt/Scripts/main/caveirahub-memesea.lua"))()
+if press then
+
 end
