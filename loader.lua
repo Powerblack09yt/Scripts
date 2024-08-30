95308e2d2d2511d846942f651e82052e109d70e3
Caveira1010 <127628592+Powerblack09yt@users.noreply.github.com>
2024-08-29 22:56:11 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,8 @@
 local placeId = game.PlaceId
 if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
     BF = true
-elseif placeId == 12411473842 then
-    press = true
+elseif placeId == 3956818381 then
+    njl = true
 end
 spawn(function()
 	while wait() do 
@@ -11,6 +11,6 @@ end)
 if BF then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Powerblack09yt/Scripts/main/Caveirahub-Bf.lua"))()
 end
-if press then
-	print("1")
+if njl then  
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Powerblack09yt/Scripts/main/ninjalegends.lua"))()
 end
