f140ecd44c0721152ce9a3204c5fb526a4fb7a37
Caveira1010 <127628592+Powerblack09yt@users.noreply.github.com>
2024-09-23 23:02:46 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,6 +3,8 @@ if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
     BF = true
 elseif placeId == 3956818381 then
     njl = true
+elseif placeId == 1537690962 then
+    bss = true
 end
 spawn(function()
 	while wait() do 
@@ -13,4 +15,8 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Powerblack09yt/Script
 end
 if njl then  
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Powerblack09yt/Scripts/main/ninjalegends.lua"))()
+if bss then  
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Powerblack09yt/Scripts/main/beeswarmsimulator.lua"))()
 end
+
+	
